8e2e47e92acea1bf6a11744e7a23fb7e6e8e65af
Borey345 <ryabovnikit@gmail.com>
2014-01-27 18:42:07 +0400

    Interference suppression works

--- interferenceOnly.m
+++ interferenceOnly.m
@@ -1,11 +1,10 @@
 %Gaussian interference
 nAntennas = 2;
-nSamples = 10000;
-interferenceToNoiseRatioDb = 20;
+nSamples = 1000;
+interferenceToNoiseRatioDb = 10;
 nSimulations = 10000;
 interferenceToNoiseRatio = 10^(interferenceToNoiseRatioDb/10);
 nEstimationSamplesValues = [2 4 6 8 10];
-% nEstimationSamplesValues = 10;
 tic
 out = zeros(nSimulations, 5);
 
@@ -15,40 +14,32 @@ sqrt2 = sqrt(2);
 
 for iValue=1:size(nEstimationSamplesValues,2)
     nEstimationSamples = nEstimationSamplesValues(iValue);
-%     nEstimationSamples = 4;
     for iSimulation = 1:nSimulations
-        INTERFERENCE = (randn(nSamples, 1) + 1i*randn(nSamples, 1))/sqrt2;
-    %     INTERFERENCE = zeros(nSamples, 1);
-%         channelCoefficients = (randn(1, nAntennas) + 1i*randn(1, nAntennas))/(5*sqrt(2));
-        channelCoefficients = (randn(1, nAntennas) + 1i*randn(1, nAntennas));
-        channelCoefficients = [1, 1i];
-        channelCoefficients = channelCoefficients/(sqrt(sum(abs(channelCoefficients).^2)));
-        Z = bsxfun(@times, channelCoefficients, INTERFERENCE);
-
-        N = noiseCoefficient*(randn(nSamples, nAntennas) + 1i*randn(nSamples, nAntennas));
-%         N = zeros(size(N));
-%         IN = 10*log10(signalPower(INTERFERENCE)/signalPower(N(:,1)))
-        X = Z+N;
-
-        %W
-
-        R = correlationMatrix(permute(X(1:nEstimationSamples,:), [2 1]));
-%         R = corrcoef(X);
-        
-        [V, D] = eig(R);
-        [~, index] = max(abs(diag(D)));
-        W = V(:, index);
-%         W(2) = -W(2);
-        YZ = Z*W;
-%         signalPower(YZ)
-        YN = N*W;
-%         YZ = Z;
-%         YN = N;
-
-
-        noisePower = signalPower(YN);
-        out(iSimulation, iValue) = 10*log10(signalPower(YZ)/noisePower+1);
-    end
+         try
+            INTERFERENCE = (randn(1, nSamples) + 1i*randn(1, nSamples))/sqrt2;
+            
+            channelCoefficients = (randn(nAntennas, 1) + 1i*randn(nAntennas, 1));
+            channelCoefficients = channelCoefficients./(abs(channelCoefficients));
+            Z = bsxfun(@times, channelCoefficients, INTERFERENCE);
+            
+            N = noiseCoefficient*(randn(nAntennas, nSamples) + 1i*randn(nAntennas, nSamples));
+            X = Z+N;
+            
+            R = correlationMatrix(X(:,1:nEstimationSamples));
+            [V, D] = eig(R);
+            [~, index] = min(abs(diag(D)));
+            W = V(:, index);
+            YZ = W'*Z;
+            YN = W'*N;
+            
+            noisePower = signalPower(YN);
+            out(iSimulation, iValue) = 10*log10(signalPower(YZ)/noisePower+1);
+        catch err
+            iValue
+            iSimulation
+            err.rethrow
+        end
+    end 
 end
 toc
 result = mean(out, 1);
